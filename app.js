65b2ac568dfda24e9697bf7f0cc4e73b571495c0
AvidDabbler <walter.k.jenkins@gmail.com>
2020-04-02 19:46:21 -0500

    Pulling in vehicle numbers and start of search

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //STATIC URLS
-const survey123Url = 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/arcgis/rest/services/survey123_76494a3414534ab482f28ced119f1f0e_fieldworker/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+const survey123Url = 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/arcgis/rest/services/survey123_7c0c3ea3fb6b4fa986769afb9ae45e5e_fieldworker/FeatureServer/0/query?where=1%3D1&outFields=CreationDate,EditDate,Editor,requested_cleaning_areas,requested_cleaning_areas_other,vehicle_number,badge_number,cleaning_datetime,completed&outSR=4326&f=json'
 
 //HTML SECTION SELECTORS
 const list_div = document.getElementById('list');
@@ -38,10 +38,10 @@ const render = async () => {
     //HTML
     //PROCESSING 
     const sorted_data = data.sort(function (a, b) {
-        if (a.attributes.new_requesting_facility <b.attributes.new_requesting_facility) {
+        if (a.attributes.vehicle_number <b.attributes.vehicle_number) {
             return -1;
         }
-        if (b.attributes.new_requesting_facility <a.attributes.new_requesting_facility) {
+        if (b.attributes.vehicle_number <a.attributes.vehicle_number) {
             return 1;
         }
         return 0;
@@ -51,19 +51,19 @@ const render = async () => {
 
     sorted_data.forEach(element => {
         // FIELDS
-        const new_requesting_facility = element.attributes.new_requesting_facility;
+        const vehicle_number = element.attributes.vehicle_number;
         const requesting_facility = element.attributes.requesting_facility;
         const field_2 = element.attributes.field_2;
 
 
-        const confirmationURL = `https://survey123.arcgis.com/share/9c92a1442f8b49faa8d5c8da83e581be?field:requesting_facility=${new_requesting_facility}`
+        // const confirmationURL = `https://survey123.arcgis.com/share/9c92a1442f8b49faa8d5c8da83e581be?field:requesting_facility=${new_requesting_facility}`
         // https://survey123.arcgis.com/share/1cb28b212b5542acbbdbaa35feba0765?field:submittedBy=Fernando%20Paredes
         
         
         list_div.innerHTML += 
-            `<div id='${new_requesting_facility}' class='button_popup fl w-100 '> 
-                <a class='openpop center fl w-100 link dim br2 ph3 pv2 mb2 dib white bg-blue' data-url="${confirmationURL}">
-                    <h2 class='f3 helvetica fl w-100'>${new_requesting_facility}</h2>
+            `<div id='${vehicle_number}' class='button_popup fl w-100 '> 
+                <a class='openpop center fl w-100 link dim br2 ph3 pv2 mb2 dib white bg-blue'>
+                    <h2 class='f3 helvetica fl w-100'>${vehicle_number}</h2>
                 </a>
             </div>`
     
@@ -109,4 +109,4 @@ const clickEvent = (event) => {
 
 
 render();
-window.addEventListener("click", clickEvent, false)
+window.addEventListener("submit", clickEvent, false)
