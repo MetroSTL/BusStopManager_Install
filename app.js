385114182768f26c6df8c37f0abcb3385dd96721
AvidDabbler <walter.k.jenkins@gmail.com>
2020-05-19 16:57:17 -0500

    initial search working

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import { jsonURL as jsonURL, surveyID as surveyID } from './private.js'; 
 
 //STATIC URLS
-const survey123Url = jsonURL();
-console.log(jsonURL)
-
 //HTML SECTION SELECTORS
 const list_div = document.getElementById('list');
 const item = document.querySelector('button_popup');
 const button_popup = document.querySelector('.button_popup');
+let stop_search = document.getElementById('search').value;
+
+
 
 // JAVASCRIPT VARIABLES
-let vehicles, filtered_vehicles;
+let stops, filtered_stops;
 let filtered = false;
 
 //POLYFILLS
@@ -27,44 +27,37 @@ if (!Element.prototype.matches) {
 }};
 
 // DATA PROCESSING
-const filter_data = (data, input) => {
-    const d = data.filter(d => {
-        return new RegExp('^' + input.replace(/\*/g, '.*') + '$').test(d.properties.vehicle_number)
-        // return d.features.properties.vehicle_number == `${input}*`
-    });
-    filtered_vehicles = d;
-    filtered = true;
-    return d
-}
+// const filter_data = (data, input) => {
+//     const d = data.filter(d => {
+//         return new RegExp('^' + input.replace(/\*/g, '.*') + '$').test(d.properties.Stp_id)
+//         // return d.features.properties.stop_number == `${input}*`
+//     });
+//     filtered_stops = d;
+//     filtered = true;
+//     return d
+// }
 
-const get_survey_data = async () => {
-    console.log('fetch');
-    const response = await fetch(survey123Url);
-    console.log(response);
-
+const get_survey_data = async (id) => {
+    const response = await fetch(jsonURL(id));
     const json = await response.json();
-    console.log(json);
-
-    let data = json.features;
-    console.log('get_survey_data', data);
-    vehicles = data;
+    let data = json.features;    stops = data;
     return data;
 }
 
 const clear_data = async () => {
     list_div.innerHTML = '';
+    document.getElementById('search').value = '';
     return;
 }
 
 
 const render = async (d) => {
-    list_div.innerHTML = 'Data is loading...';
-    // SORTED DATA BY VEHICLE NUMBER
+    // SORTED DATA BY stop NUMBER
     const sorted_data = d.sort(function (a, b) {
-        if (a.properties.vehicle_number <b.properties.vehicle_number) {
+        if (a.properties.STP_ID <b.properties.STP_ID) {
             return -1;
         }
-        if (b.properties.vehicle_number <a.properties.vehicle_number) {
+        if (b.properties.STP_ID <a.properties.STP_ID) {
             return 1;
         }
         return 0;
@@ -72,38 +65,14 @@ const render = async (d) => {
     
     list_div.innerHTML = '';
     if(sorted_data.length == 0){
-        list_div.innerHTML = 'No vehicles with that number';
+        list_div.innerHTML = 'No stops with that number';
     }else{
         sorted_data.forEach(element => {
-            let curTime = new Date();
-
-            let cleaningTime = 2 * 60 * 60 * 1000;
-
-            let date = new Date(element.properties.cleaning_datetime);
-            let newDate = (date) => {
-                let tod = ' AM';
-                let hours = date.getHours()
-                if(date.getHours() > 12){
-                    hours = hours - 12
-                    tod = ' PM'
-                }
-            return date.getMonth() + '/' + date.getDay() + '/' + date.getFullYear() + ' ' + hours + ':' + date.getMinutes() + tod;
-            }
-
-            let color = () => {
-                if((curTime - date) > cleaningTime){
-                    return 'bg-red';
-                }else{
-                    return 'bg-blue';
-                }
-
-            }
-
             list_div.innerHTML += 
-                `<div id='${element.properties.vehicle_number}' class='button_popup fl w-100 '>
-                    <a data-oid = ${element.properties.objectid} class='openpop center fl w-100 link dim br2 ph3 pv2 mb2 dib white ${color()}'>
-                        <h2 class='fl f3 helvetica fl'>Vehicle: ${element.properties.vehicle_number}</h2>
-                        <h2 class='fl f3 helvetica fl'>   Last Cleaned: ${newDate(date)}</h2>
+                `<div id='${element.properties.STP_ID}' class='button_popup fl w-100 '>
+                    <a data-oid = ${element.properties.objectid} 
+                    class='openpop center fl w-100 link dim br2 ph3 pv2 mb2 dib white bg-blue'>
+                        <h2 class='fl f3 helvetica fl'>Stop ID: ${element.properties.STP_ID} Stop Name: ${element.properties.STP_NAME}</h2>
                     </a>
                 </div>`;
         }
@@ -114,61 +83,33 @@ const render = async (d) => {
 
 const clickEvent = async (event) => {
     event.preventDefault();
+    stop_search = document.getElementById('search').value
     const iframe_exists = document.getElementById('ifrm');
     const iframe = event.target.closest('#iframe');
     const search = event.target.closest('#search');
-    const vehicle_search = document.getElementById('vehicle').value;
-    
+
     const searching = async (value) => {
         list_div.innerHTML = 'Data is loading...';
         clear_data();
         filtered = true;
-        let fv = filter_data(vehicles, `${value}*`);
         render(await fv);
         return;
     };
 
-    // CLOSE IFRAME / CLICK OFF IFRAME WHEN ITS OPEN
-    if(!iframe && iframe_exists){
-        
-        // CLOSE IFRAME
-        iframe_exists.parentNode.removeChild(iframe_exists);
+    console.log('search!!!')
+    console.log(event)
+    if(stop_search == ''){
+
         return;
-        
-    // SEARCH CLICK!!!
-    }else if(search){
-        if(vehicle_search != ''){
-            searching(vehicle_search);
-        }else if(vehicle_search == '' && filtered){
-            render(vehicles)
-            return;
-        }
-    // CLICK LIST ELEMENT AND OPEN IFRAME!!!
-    }else if (event.target.closest('.openpop')){        
-        let item = event.target.closest('.openpop');
-        console.log(item);
-        let url = `https://survey123.arcgis.com/share/${surveyID()}?mode=edit&objectId=${item.getAttribute('data-oid')}&version=latest`;
-        const ifrm = document.createElement('iframe');
-        const el = document.getElementById('marker');
-        const main = document.querySelector('#main');
-
-        ifrm.setAttribute('id', 'ifrm'); // assign an id
-        ifrm.setAttribute(`src`, url);
-
-        // // to place before another page element
-        main.parentNode.insertBefore(ifrm, el);
-
-        // // assign url
-        // ifrm.setAttribute('src', 'demo.html');
-        
+    }else{
+        console.log('here')
+        get_survey_data(stop_search).then(data => render(data))
         return;
     }
-}
+};
 
-get_survey_data().then(data =>{
-    render(vehicles);
-});
+clear_data();
 
 window.addEventListener("submit", clickEvent, false)
-window.addEventListener("click", clickEvent, false)
+
 
--- private.js
+++ private.js
@@ -1,5 +1,6 @@
-const jsonURL = () => {
-    return 'https://opendata.arcgis.com/datasets/736adfbb680d4fe696499553d90be5d1_0.geojson'
+const jsonURL = (id) => {
+    console.log(id);
+    return  `https://services2.arcgis.com/ZV8Mb62EedSw2aTU/ArcGIS/rest/services/Stop_Reassessment_200207_view/FeatureServer/0/query?where=cast%28STP_ID+as+varchar%2810%29%29+LIKE+%27%25${id}%25%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=iLw5_zNF2-ivmuHYXRdBqwqhGbt_aAgz4CXTk_pdDoK5Fh02yf-nSOjajkb-S4xmDEARmB9tjfdkEEJSyQeazSz3AnYo5LvE8bjSpkwqE1-f4h5T4XaURlxk6GD83uwYe6-YiuY_QvicGtHQ0Rb2HValhPLrAFN8-MksjifIWdJk71Urm0tTPwegq50LrXU8yD1Tm4pgXLsnal1vxrPw-ql1UQVKiHhL7CVbz0sYxUo2nxn5is0WEwF1UNzfg-86tfszIuuTWgXPIMr4oRcYFw..`
 }
 
 const surveyID = () => {
