ad622a56114b2fd1faa8e00c0439504f997020e3
AvidDabbler <walter.k.jenkins@gmail.com>
2020-06-05 15:03:29 -0500

    update to assessment issues update url params

--- app.js
+++ app.js
@@ -11,22 +11,14 @@ import { jsonURL, surveyID, surveyData } from "./private.js";
 //STATIC URLS
 //HTML SECTION SELECTORS
 const list_div = document.getElementById("list");
-const item = document.querySelector("button_popup");
-const button_popup = document.querySelector(".button_popup");
 let stop_search = document.getElementById("search").value;
-const init = document.getElementById("init-search");
-const stopButton = document.getElementById("main");
-let searchData;
 
-// JAVASCRIPT VARIABLES
-let stops, filtered_stops;
-let filtered = false;
+// generic lists for putting list of stopID's
 let assessments = {
     failed: [],
     approved: [],
     pending: [],
 };
-let failedList = [];
 
 //POLYFILLS
 if (!Element.prototype.matches) {
@@ -50,7 +42,6 @@ const get_survey_data = async (url) => {
   const response = await fetch(url);
   const json = await response.json();
   let data = json.features;
-  stops = data;
   return data;
 };
 
@@ -62,25 +53,28 @@ const clear_data = async () => {
 };
 
 // uses assessment status and returns color for css
-const setStatus = (stop, version) => {
+const setStatus = (stop) => {
     console.log(stop)
     let obj;
-    if (version == 'assess') {
+        // failed renders red
         if (assessments['failed'].includes(String(stop.attributes.stopID))) {
             obj = {
                 color: 'red',
                 link: '',
             };
+        // approved renders green
         } else if (assessments['approved'].includes(String(stop.attributes.stopID))) {
             obj = {
                 color: 'green',
                 link: '',
             };
+        // pending renders orange
         } else if (assessments['pending'].includes(String(stop.attributes.stopID))) {
             obj = {
                 color: 'orange',
                 link: '',
             };
+        // renders unaddressed as blue
         } else {
             obj = {
                 color: 'blue',
@@ -88,138 +82,87 @@ const setStatus = (stop, version) => {
             }
         };
 
-        let base =
-            `<div id='${stop.attributes.stopID}' class='button_popup fl w-100 '>
-                <a
-                    data-objectid = '${stop.attributes.objectid}'
-                    data-globalid = '${stop.attributes.globalid}'
-                    data-stopid = '${stop.attributes.stopID}'
-                    data-assessStatus = '${stop.attributes.approved}'
-                    data-approvalComments = '${stop.attributes.approvalComments}'
-    
-                    class='openpop center fl w-100 link dim br2 ph3 pv2 mb2 dib white bg-${obj.color}'>
-                    <ul>
-                        <li class='f3 helvetica'><b>Stop ID:</b> ${stop.attributes.stopID}
-                        </li>
-                        <li class='f3 helvetica'><b>Stop Name:</b> ${stop.attributes.stopName}
-                        </li>    
-                    </ul>
-                </a>
-            </div>`;
-        return base;
-    }
-    else if (version == "master") {
-        if (assessments['failed'].includes(String(stop.properties.STP_ID))) {
-            obj = {
-                color: 'red',
-                link: '',
-            };
-        } else if (assessments['approved'].includes(String(stop.properties.STP_ID))) {
-            obj = {
-                color: 'green',
-                link: '',
-            };
-        } else if (assessments['pending'].includes(String(stop.properties.STP_ID))) {
-            obj = {
-                color: 'orange',
-                link: '',
-            };
-        } else {
-            obj = {
-                color: 'blue',
-                link: '',
-            }
-        }
-        let base =
-            `<div id='${stop.properties.STP_ID}' class='button_popup fl w-100 '>
-                    <a 
-                        data-stopID = '${stop.properties.STP_ID}'
-                        data-onSt = '${stop.properties.ON_ST}'
-                        data-atSt = '${stop.properties.AT_ST}'
-                        data-stopName = '${stop.properties.STP_NAME}'
-                        data-installInfo = '${stop.properties.LocChange}'
-                        data-routes = '${stop.properties.ROUTES}'
-                        data-tParkPoles ='${stop.properties.TParkPoles}'
-                        data-tParkSigns = '${stop.properties.TNPSigns}'
-                        data-busStopPole = '${stop.properties.TStopPoles}'
-                        data-instPos = '${stop.properties.InstPos}'
-                        data-stdInstLoc = '${stop.properties.StdInLoc}'
-                        data-parkSignRear = '${stop.properties.NPSign1}'
-                        data-parkSignNSFront = '${stop.properties.NPSign2}'
-                        data-parkSignMBFront = '${stop.properties.NPSign3}'
-                        data-parkSignFarside = '${stop.properties.NPSign4}'
-                        data-gpsLat = '${stop.geometry.coordinates[0]}'
-                        data-gpsLon = '${stop.geometry.coordinates[1]}'
+        return `<div id='${stop.attributes.stopID}' class='button_popup fl w-100 '>
+                    <a
+                        data-objectid = '${stop.attributes.objectid}'
+                        data-globalid = '${stop.attributes.globalid}'
+                        data-stopid = '${stop.attributes.stopID}'
+                        data-assessStatus = '${stop.attributes.approved}'
+                        data-approvalComments = '${stop.attributes.approvalComments}'
+        
                         class='openpop center fl w-100 link dim br2 ph3 pv2 mb2 dib white bg-${obj.color}'>
-                        
                         <ul>
-                            <li class='f3 helvetica'><b>Stop ID:</b> ${stop.properties.STP_ID}</li>
-                            <li class='f3 helvetica'><b>Stop Name:</b> ${stop.properties.STP_NAME}</li>    
+                            <li class='f3 helvetica'><b>Stop ID:</b> ${stop.attributes.stopID}
+                            </li>
+                            <li class='f3 helvetica'><b>Stop Name:</b> ${stop.attributes.stopName}
+                            </li>    
                         </ul>
                     </a>
-            </div>`;
-        return base;
-    }
+                </div>`;
+
 };
 
-const render = async function (d, version) {
+// sort and render stops from specified list
+const render = async function (d) {
     console.log(d);
   // SORTED DATA BY stop NUMBER
     const sorted_data = await d.sort(function (a, b) {
-        if (a.properties.stopID < b.properties.stopID) {
+        if (a.attributes.stopID < b.attributes.stopID) {
         return -1;
         }
-        if (b.properties.stopID < a.properties.stopID) {
+        if (b.attributes.stopID < a.attributes.stopID) {
         return 1;
         }
         return 0;
     });
 
     list_div.innerHTML = "";
-    if (version == "assess") {
-        await sorted_data.forEach((element) => {
-            list_div.innerHTML += setStatus(element, 'assess');
-        });
-    } else {
-        await sorted_data.forEach((element) => {
-            if(assessments['failed'].includes(String(item.dataset.stopid)) ||
-                assessments['pending'].includes(String(item.dataset.stopid))) {
-                    list_div.innerHTML += ''
-            } else {
-                list_div.innerHTML += setStatus(element, 'master');
-                }
-        });
-        
-    }
+    await sorted_data.forEach((element) => {
+        list_div.innerHTML += setStatus(element, 'assess');
+    });
     
 };
 
+// html filler for showing data is loading
 const dataLoading =  '<h2 class="i">Data is loading...</h2>';
 
+// search and render stops (generic function)
 const searching = (stop_search, version) => {
     list_div.innerHTML = dataLoading;
     get_survey_data(stop_search).then((data) => {
     render(data, version);
-    searchData = data;
   });
 };
 
+// adds stop id's to pending, approved, or failed lists
 const getAssessments = (url) => {
     get_survey_data(url).then((data) => { 
+        console.log(data.length)
         data.forEach(el => {
-            if (el.attributes.approved == "no" || el.attributes.approvalComments != null){
-                document.getElementById('notification-icon').src = 'assets/notification2.svg';
+            // install has not been filled out
+            if (el.attributes.approved == "no" || el.attributes.approvalComments != null) {
                 assessments['failed'].push(el.attributes.stopID);
+            // if install was approved
             } else if (el.attributes.approved == "yes" ) {
                 assessments['approved'].push(el.attributes.stopID);
-            } else if (el.attributes.approved == null) {
-                document.getElementById('mailbox-icon').src = 'assets/mailbox2.svg';
+            // if stop is pending approval
+            } else if (el.attributes.approved == null && el.attributes.installToSpec != null) {
+                console.log(el.attributes.installToSpec)
                 assessments['pending'].push(el.attributes.stopID);
+
             }
         })
-    });
+        return assessments
+    }).then(assessments => {
+        console.log(assessments);
+        if (assessments.failed.length > 0) {
+            document.getElementById('notification-icon').src = 'assets/notification2.svg';}
+        if (assessments.pending.length > 0) {
+            document.getElementById('mailbox-icon').src = 'assets/mailbox2.svg';}
+    })
 };
 
+// generic function that takes a type (pending, approved, failed) and renders only those stops
 const getIssues = async (type) => {
     list_div.innerHTML = dataLoading;
     const crossRef = async () => {
@@ -234,10 +177,16 @@ const getIssues = async (type) => {
     };
     crossRef().then(async data => {
         let div = '';
+        // waits 3 seconds to 
         setTimeout(async () => {
+            // renders if there are no stops that match that parameter
+            if (data.length == 0) {
+                list_div.innerHTML = '<h2 class="i">No Stops Match that Parameter</h2>';
+            }
+            // renders list of stops from the type list
             await data.forEach(async (element) => {
                 list_div.innerHTML = ''
-                list_div.innerHTML += setStatus(element, 'assess');
+                list_div.innerHTML += setStatus(element);
                 return await div;
             })
             
@@ -250,9 +199,10 @@ const clickEvent = async (event) => {
     const iframe_exists = document.getElementById("ifrm");
     stop_search = document.getElementById("search").value;
     const iframe = event.target.closest("#iframe");
-    const search = event.target.closest("#search");
+    // const search = event.target.closest("#search");
     const notification = event.target.closest('#notification-icon');
     const mailbox = event.target.closest('#mailbox-icon');
+    const completed = event.target.closest('#completed-icon');
 
     let item = event.target.closest(".openpop");
     
@@ -261,7 +211,7 @@ const clickEvent = async (event) => {
         (event.type == 'click' && event.target.closest('#search')))
         && stop_search != ""
         && !iframe_exists) {
-            searching(jsonURL(stop_search).master, "master");
+            searching(jsonURL(stop_search).assess);
             return;
     }
   // CLOSE IFRAME / CLICK OFF IFRAME WHEN ITS OPEN
@@ -276,43 +226,14 @@ const clickEvent = async (event) => {
     } else if (mailbox) {
         getIssues('pending');
         return;
-    // SEARCH CLICK!!!
-    // } else if (search) {
-    //     if (stop_search != "") {
-    //         searching(stop_search, 'master');
-    //     } 
+    } else if (completed) {
+        getIssues('approved');
+        return;
+
         // CLICK LIST ELEMENT AND OPEN IFRAME!!!
     } else if (event.target.closest(".openpop")) {
-        let url = () => {
-            let link = ``;
-            if (assessments['failed'].includes(String(item.dataset.stopid)) ||
-                assessments['pending'].includes(String(item.dataset.stopid))) {
-                link += `https://survey123.arcgis.com/share/${surveyID()}?mode=edit&objectId=${item.dataset.objectid}`    
-            } else if (assessments['approved'].includes(String(item.dataset.stopid))) {
-                link = '';
-
-            } else {
-                link += `https://survey123.arcgis.com/share/${surveyID()}
-                    ?field:stopID=${item.dataset.stopid}
-                    &field:onSt=${item.dataset.onst} 
-                    &field:atSt=${item.dataset.atst} 
-                    &field:stopName=${item.dataset.stopname} 
-                    &field:installInfo=${item.dataset.installinfo} 
-                    &field:routes=${item.dataset.routes} 
-                    &field:tParkPoles=${item.dataset.tparkpoles} 
-                    &field:tParkSigns=${item.dataset.tparksigns} 
-                    &field:installSurface=${item.dataset.installsurface}
-                    &field:installBusStopPole=${item.dataset.installpole == "0" ? "no" : "yes"}
-                    &field:assignedPos=${item.dataset.instpos} 
-                    &field:parkSignRear=${item.dataset.parksignrear} 
-                    &field:parkSignNSFront=${item.dataset.parksignnsfront} 
-                    &field:parkSignMBFront=${item.dataset.parksignmbfront} 
-                    &field:parkSignFarside=${item.dataset.parksignfarside} 
-                    &field:gpsLat${item.dataset.gpslat}
-                    &field:gpsLat${item.dataset.gpslon}`;
-            }
-            return link;
-        };
+        let url = () => `https://survey123.arcgis.com/share/${surveyID()}?mode=edit&objectId=${item.dataset.objectid}`; 
+            
         let href = url();
         console.log(href);
         if (href == "") {
