cf151d6fde4a6d904d0b87ab4443279ceafdb99f
Walter Jenkins <walter.k.jenkins@gmail.com>
2020-12-30 13:43:31 -0600

    updated private for feature class query

--- private.js
+++ private.js
@@ -1,45 +1,9 @@
-let assessment = 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/ArcGIS/rest/services/service_834b037efc2b4f58a2e6ca08298d8686/FeatureServer/0'
-let master = 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/ArcGIS/rest/services/Stop_Reassessment_200207_view/FeatureServer/0'
-let dig = 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/ArcGIS/rest/services/service_7a70150209ac48488446b15da0583857/FeatureServer/0'
-
-
-
-const jsonURL = (id, token) => {
-    // returns only the ids that contain the stopid string that was inputed (WHERE STP_ID LIKE '%input%')
-    let assessmentField = 'stopID';
-    let masterField = 'STP_ID';
-    return {
-        assess: `${assessment}/query?where=cast%28${assessmentField}+as+varchar%2810%29%29+LIKE+%27%25${id}%25'&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=${token}`,
-        master: `${master}/query?where=cast%28${masterField}+as+varchar%2810%29%29+LIKE+%27%25${id}%25%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=${token}`,
-    }
-};
-
-const surveyData = (token) => {
-    return {
-        assess: `${assessment}/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=& spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=${token}`,
-        dig: `${dig}/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=${token}`
-
-
-    }
-};
-
-
 var clientId = 'hhibAhdSLrpXfARP';
-// var redirectUri = 'http://127.0.0.1:8080/';
-var redirectUri = 'https://externalapps.metrostlouis.org/busstopassessment';
+var redirectUri = 'http://127.0.0.1:5500/';
+// var redirectUri = 'https://externalapps.metrostlouis.org/bsm-install';
 
-// do this on a button click to avoid popup blockers
+const jsonURL = (id, token) => `https://metroas08.metrostlouis.org/arcserver/rest/services/Hosted/Install_Survey_Results_Bus_Stop_Manager/FeatureServer/0/query?where=cast%28stopId+as+varchar%2810%29%29+LIKE+%27%25${id}%25'&%28assess_feasible+%3D+%27YES%27%29+OR+%28install_approved+<>+%27YES%27%29&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnCentroid=false&sqlFormat=none&resultType=&datumTransformation=&f=pjson&token=${token}`
 
+const survey_id = 'ad1421bb1d224e129752bba181588dc9'
 
-// requests log
-// https://itic.occinc.com/wsexternal/service/getticketstatus/BiStateDevelopment/W3lc0me!/MO/1
-
-
-const surveyID = () => {
-    return {
-        assess: '5e2cad9331d3458583bea6da5f19e488',
-        dig: '62060dec722640ae97ac2c5653950429',
-    }
-}
-
-export { jsonURL, surveyID, surveyData, clientId, redirectUri }
+export { jsonURL, install_config, clientId, redirectUri }
