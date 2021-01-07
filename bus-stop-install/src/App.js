362229a86f06d00248aaba835189b04112375edc
Walter Jenkins <walter.k.jenkins@gmail.com>
2021-01-07 14:50:53 -0600

    updated App.js with useEffect

--- bus-stop-install/src/App.js
+++ bus-stop-install/src/App.js
@@ -2,35 +2,48 @@ import logo from './logo.svg';
 import './tachyons.min.css';
 import './App.css';
 import Search from './components/Search';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import loginIcon from './assets/login.svg';
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
   const [user, setUser] = useState('');
-  let token = '';  
-  const fullHash = document.location.hash.split('#')[1];
-  if (fullHash) {
-      const params = JSON.parse('{"' + decodeURI(fullHash).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
-      token = params.access_token
-  }
-
+  const [loaded, setLoaded] = useState(false);
+  const [token, setToken] = useState('');
+  
   const load = () => {
+    const fullHash = document.location.hash.split('#')[1];
 
-  }
+    if (fullHash) {
+      const params = JSON.parse('{"' + decodeURI(fullHash).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+      setSignedIn(true);
+      setUser(params.username.replace('%40','@'));
+      return params.access_token
+    } else {
+      return '';
+    }
+  };
   
   const getToken = () => {
     var redirectUri = 'http://127.0.0.1:3000/';
     var clientID = 'ClUjM15qkXs1GSHA';
-
+    
     window.location.href = 'https://metroas08.metrostlouis.org/arcgis/sharing/rest/oauth2/authorize?client_id=' + clientID + '&response_type=token&redirect_uri=' + window.encodeURIComponent(redirectUri) + 'oauth-window' +'height=400,width=600,menubar=no,location=yes,resizable=yes,scrollbars=yes,status=yes';
+    
+  };
 
-  } 
+  useEffect(() => {
+    setToken(load());
+    console.log(user)
+    
+
+  }, [])
+  
 
   return (
       <div className="App">
         <div id="top-bar" class="w-100 flex">
-            <h3>{}</h3>
+            <h3>{token == '' ? '' : user}</h3>
 
             <button id="sign-in" class="w-20 items-center ml-auto" onClick={getToken}>
                 <img id="sign-in-icon" src={loginIcon} alt='signin' />
